8eb6ff54c9e1af74546eb3fd46dec4cea45b9f1b
CherryJie <jie.zhou@daocloud.io>
2018-07-19 00:36:12 +0800

    页面的初始化，及骨架页面的安排

---
+++ vue-music/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from './components/m-header/m-header'
+export default {
+  name: 'App',
+  components: {
+    MHeader,
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
