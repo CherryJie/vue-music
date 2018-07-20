b0e7deb6e65249c14c13ba7e2276104303ff6751
CherryJie <jie.zhou@daocloud.io>
2018-07-20 10:23:49 +0800

    初步完成 jsonp 的实现

--- vue-music/build/webpack.base.conf.js
+++ vue-music/build/webpack.base.conf.js
@@ -37,6 +37,7 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js',
       'src': resolve('src'),
       'common': resolve('src/common'),
+      'api': resolve('src/api'),
       'components': resolve('src/components'),
     }
   },
--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Search from 'components/search/search';
+import Singer from 'components/singer/singer';
+import Rank from 'components/rank/rank';
+import Recommend from 'components/recommend/recommend';
 
 // 注册
 Vue.use(Router);
@@ -8,8 +12,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      // name: 'HelloWorld',
-      // component: HelloWorld,
+      redirect: '/recommend',
+    },
+    {
+      path: '/rank',
+      component: Rank,
+    },
+    {
+      path: '/recommend',
+      component: Recommend,
+    },
+    {
+      path: '/singer',
+      component: Singer,
+    },
+    {
+      path: '/search',
+      component: Search,
     },
   ],
 });
